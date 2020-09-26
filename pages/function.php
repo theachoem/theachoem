1e2d3912ceb4124cf348a16c4eaa9d5d0ac63832
Thea Choem <theacheng.g6@gmail.com>
2020-09-27 01:19:55 +0700

    update portfolio image

--- pages/function.php
+++ pages/function.php
@@ -8,8 +8,8 @@
 
     //C:/Users/theac/.gitconfig
     //git config --add --global alias.ph "echo ' ' > .gitignore && git add . && git commit -m '$1' && shift && git push heroku master && echo 'pages/ignore/' > .gitignore && git rm -r --cached . && git add . && git commit -m '$1' && git push origin master"
-    //echo "" > .gitignore && git add . && git commit -m 'push to heroku' && git push heroku main && echo 'pages/ignore/' > .gitignore && git rm -r --cached . && git add . && git commit -m 'push to heroku' && git push origin main
-    //rm -rf .git && git init && git add . && git commit -m "init commit" && git remote add origin https://github.com/theacheng/theacheng.git && git push --mirror --force
+    //echo "" > .gitignore && git add . && git commit -m 'update portfolio image' && git push heroku main && echo 'pages/ignore/' > .gitignore && git rm -r --cached . && git add . && git commit -m 'update portfolio image' && git push origin main
+    //rm -rf .git && git init && git add . && git commit -m "init" && git remote add origin https://github.com/theacheng/theacheng.git && git push --mirror --force
 
     //ALTER TABLE `works` ADD `date` DATE NOT NULL AFTER `description`; 
     //variable declaration

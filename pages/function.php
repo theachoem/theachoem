eb3fe55c7f80df31ce8a92094bd1a17e2dafa6fd
Thea Choem <theacheng.g6@gmail.com>
2020-09-29 21:07:32 +0700

    fixing bug

--- pages/function.php
+++ pages/function.php
@@ -10,7 +10,7 @@
     //rm -rf .git && git init && git add . && git commit -m "init" && git remote add origin https://github.com/theacheng/theacheng.git && git push --mirror --force
 
     //git config --add --global alias.ph "echo ' ' > .gitignore && git add . && git commit -m '$1' && shift && git push heroku master && echo 'pages/ignore/' > .gitignore && git rm -r --cached . && git add . && git commit -m '$1' && git push origin master"
-    //echo "" > .gitignore && git add . && git commit -m 'add admin editor' && git push heroku main && echo 'pages/ignore/' > .gitignore && git rm -r --cached . && git add . && git commit -m 'add admin 9/29/2020' && git push origin main
+    //echo "" > .gitignore && git add . && git commit -m 'fixing bug' && git push heroku main && echo 'pages/ignore/' > .gitignore && git rm -r --cached . && git add . && git commit -m 'fixing bugs' && git push origin main
    
     //ALTER TABLE `works` ADD `date` DATE NOT NULL AFTER `description`; 
     //variable declaration
